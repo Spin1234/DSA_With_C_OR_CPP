3c4e96a9aab6f87cd0b6a77956664722333dc9f4
Sayak Pal <121346103+Spin1234@users.noreply.github.com>
2024-07-17 21:26:39 +0530

    Create LeftRotation.cpp

---
+++ Array/Left Rotation/LeftRotation.cpp
@@ -0,0 +1,10 @@
+vector<int> rotateLeft(int d, vector<int> arr) {
+    vector<int> ans;
+    for(int i=d;i<arr.size();i++){
+        ans.push_back(arr[i]);
+    }
+    for(int i=0; i<d; i++){
+        ans.push_back(arr[i]);
+    }
+    return ans;
+}
